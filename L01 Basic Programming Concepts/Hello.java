4057478e856147a28bad245da78347d247afeb03
alanpolun <113871475+alanpolun@users.noreply.github.com>
2023-09-04 08:50:08 +0000

    Fix Error

--- L01 Basic Programming Concepts/Hello.java
+++ L01 Basic Programming Concepts/Hello.java
@@ -1,9 +1,5 @@
 public class Hello {
-<<<<<<< HEAD
-    public static void main(String[] args){        
-=======
     public static void main(String[] args){
->>>>>>> 3ab5356b3dcd1d62b643c75e7fe1732643a52cf2
         System.out.println("Hello Alan Po!");
     }
 }
