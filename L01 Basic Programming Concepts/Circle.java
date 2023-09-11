62ed700e228cda80925d69d8d1c08c055c1a6f39
alanpolun <113871475+alanpolun@users.noreply.github.com>
2023-09-11 06:53:24 +0000

    L02 variables

--- L01 Basic Programming Concepts/Circle.java
+++ L01 Basic Programming Concepts/Circle.java
@@ -1,12 +1,13 @@
 public class Circle {
-    public static void main(String[] args){
-        int	radius;  //variables
-		double area;
+	public static void main(String[] args) {
 
-		radius = 5;
-		System.out.println("Radius = " + radius);
-
-		area = radius * radius * 3.14;
-		System.out.println("Area = " + area);
-    }
+		int i = 10;
+		int k = 7;
+		int j = k++ + ++i; //i += 1
+		System.out.println(i);
+		System.out.println(k);
+		System.out.println("j = "+j);
+		--i; //i -= 1
+		System.out.println(i);
+	}
 }
