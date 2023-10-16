063cd9a384cfdc7d78277a74e9dd0650252aab81
alanpolun <113871475+alanpolun@users.noreply.github.com>
2023-10-16 09:16:41 +0000

    L2.5 codes updated after 16 Nov Lecture

--- L2.5 Array/L25Slide14.java
+++ L2.5 Array/L25Slide14.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class L25Slide14 {
 	public static void main(String [ ] args) {
-		int[][] myTable = { 	{89, 76, 6, 85, 4},
-								{56, 24, 52, 90, 85},
+		int[][] myTable = { 	{89, 76, 6},
+								{56, 24, 52, 90, 85, 89, 66},
 								{3, 11, 34, 23, 62},
-								{19, 63, 18, 9, 11} };
+								{19, 63, 18, 9} };
 
 		for (int row=0; row<myTable.length; row++){
 			for (int col=0; col<myTable[row].length; col++){ 
--- L2.5 Array/L25Slide3.java
+++ L2.5 Array/L25Slide3.java
@@ -2,13 +2,19 @@ import java.util.*;
 public class L25Slide3{
 	public static void main(String [ ] args) {
 		Scanner 	keyboard = new Scanner(System.in);
-		int 		grade1, grade2, grade3, grade4;
-		double 	average;
-		grade1 = keyboard.nextInt();
-		grade2 = keyboard.nextInt();
-		grade3 = keyboard.nextInt();
-		grade4 = keyboard.nextInt();
-		average = (grade1 + grade2 + grade3 + grade4) / 4.0;
+		//int 		grade1, grade2, grade3, grade4, grade5;
+		int[] grade;
+		int numOfgrade;
+		System.out.print("How many students? ");
+		numOfgrade = keyboard.nextInt();
+		grade = new int[numOfgrade];
+		double 	average, sum=0;
+		for(int i=0; i<grade.length; i++){
+			grade[i] = keyboard.nextInt();
+			sum += grade[i];
+		}
+		
+		average = sum / grade.length;
 		System.out.println("Average = " + average);
 	}
 }	
